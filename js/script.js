ec39f7633973a7afd4803cdaddb8baba5cd87065
mrota89 <marcello.rota.1989@gmail.com>
2021-02-18 23:41:37 +0100

    completato jumbotron e parte iniziale del main

--- js/script.js
+++ js/script.js
@@ -2,6 +2,40 @@ new Vue({
   el: '#app',
   data: {
     menuNav:['HOME', 'COURSES', 'INSTRUCTORS', 'EVENTS', 'PAGES', 'ELEMENTS'],
+    courses: [
+      {
+        icon: '-1',
+        name: 'Languages'
+      },
+      {
+        icon: '-2',
+        name: 'Software'
+      },
+      {
+        icon: '-3',
+        name: 'Business'
+      },
+      {
+        icon: '-4',
+        name: 'Chemistry'
+      },
+      {
+        icon: '-5',
+        name: 'Science'
+      },
+      {
+        icon: '-6',
+        name: 'DIY&Craft'
+      },
+    ],
+  },
+
+  methods: {
+    imageCourse: function(index) {
+      const icon = this.courses[index].icon;
+      const imageString = `img/h5-custom-icon${icon}.png`;
+      return imageString;
+    }
   }
 
 });//end vue app
